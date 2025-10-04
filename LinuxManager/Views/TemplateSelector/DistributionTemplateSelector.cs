1bd567d133490adbb7e1ba48179542914e71153f
NathanQuellec <71017057+NathanQuellec@users.noreply.github.com>
2025-10-04 21:13:29 +0200

    Migrate from ListDetailsView UI Community kit to SplitView/ListView

---
+++ LinuxManager/Views/TemplateSelector/DistributionTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using LinuxManager.Models;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LinuxManager.Views.TemplateSelector;
+
+public class DistributionTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate DefaultTemplate
+    {
+        get; set;
+    }
+    public DataTemplate DistributionTemplate
+    {
+        get; set;
+    }
+
+    protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+    {
+        return item is Distribution ? DistributionTemplate : DefaultTemplate;
+    }
+}
